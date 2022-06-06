db8dba8d0cfd3fe933ff7de27d28a6f66c67c903
David Ongaro <davidongaroh@gmil.com>
2022-06-06 23:12:52 +0300

    commit tree 611fc3adac58e28e4b2edf61f712a8621bd7cccd

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+    let num1=2
+    let num2=31
+    let multiply =num1*num2
+
+let random= Math.ceil(Math.random()*20)+2
+
+let num3= 32
+let num4=8
+let mod= num3/num4
+
+let max= Math.max(1,2,4,6,7,8,5,20,3,10,18,14,12)
